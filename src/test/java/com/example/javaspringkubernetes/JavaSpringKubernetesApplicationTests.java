1ce54a9dde77bba3484204919d8bd8eb3dd46dbb
Gildas Tema <gildastema3@gmail.com>
2022-11-11 19:27:40 +0100

    fix test #1

--- src/test/java/com/example/javaspringkubernetes/JavaSpringKubernetesApplicationTests.java
+++ src/test/java/com/example/javaspringkubernetes/JavaSpringKubernetesApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class JavaSpringKubernetesApplicationTests {
+class JavaSpringKubernetesApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
